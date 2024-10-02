716371d61da0f56f2ab810b90e905b5d18e635da
onlyBel <belindawnjr@gmail.com>
2024-10-02 00:49:03 +0000

    nodejs 5

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,9 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * message ""Hello NodeJS!"
+ */
+
 const displayMessage = (msg) => {
 	  console.log(msg);
 };
